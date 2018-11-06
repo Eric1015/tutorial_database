70d914274779eee7402daea554cc6e154271ec1c
love1 <love1dbsb77@hotmail.com>
2018-11-06 10:34:13 -0800

    Fixed the problem that controller not displaying json file

--- app/controllers/tires_controller.rb
+++ app/controllers/tires_controller.rb
@@ -1,7 +1,11 @@
 class TiresController < ApplicationController
   def index
+    @tires = Tire.all
+    render json: @tires
   end
 
   def show
+    @tire = Tire.find(params[:id])
+    render json: @tire
   end
 end
